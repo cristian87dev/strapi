84575d42730a69ed50fbf52250106bec652c20c8
Cristian Perrone <cristian.perrone@intellisite.io>
2023-03-01 14:30:54 -0300

    Created new user, added new content type

--- config/database.js
+++ config/database.js
@@ -1,13 +1,10 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
     },
+    useNullAsDefault: true,
+    debug: false,
   },
 });
